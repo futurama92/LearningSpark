27a0260f15ff5af8528d0601542746ad96fa476c
Riccardo Trivellato <riccardo.trivellato@itecons.it>
2017-12-28 10:52:31 +0100

    Add new script, fix bug logtest

--- logtest.py
+++ logtest.py
@@ -1,6 +1,6 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("My App")
+conf = SparkContext.setMaster("local").setAppName("log")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile("log.txt")
