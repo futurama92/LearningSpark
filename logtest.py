4dd59c35a6983efe77e06ced1d4ab11f70bd6416
Riccardo Trivellato <riccardo.trivellato@itecons.it>
2017-12-28 10:36:52 +0100

    Add conversion int to string

--- logtest.py
+++ logtest.py
@@ -7,5 +7,5 @@ lines = sc.textFile("log.txt")
 errorRDD = lines.filter(lambda x: "error" in x)
 warningRDD = lines.filter(lambda x: "warning" in x)
 
-print "Errori: " + errorRDD.count()
-print "Warning: " + warningRDD.count()
+print "Errori: " + str(errorRDD.count())
+print "Warning: " + str(warningRDD.count())
