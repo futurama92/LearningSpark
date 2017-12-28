246b22b731136397b4a271b401e6709800fb3d19
Riccardo Trivellato <riccardo.trivellato@itecons.it>
2017-12-28 11:06:18 +0100

    remove apache Log

--- logtest.py
+++ logtest.py
@@ -1,11 +1,14 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkContext.setMaster("local").setAppName("log")
+# Inizialize sparkContext
+conf = SparkConf().setMaster("local").setAppName("logtest")
 sc = SparkContext(conf = conf)
 
+# Operetion on file
 lines = sc.textFile("log.txt")
 errorRDD = lines.filter(lambda x: "error" in x)
 warningRDD = lines.filter(lambda x: "warning" in x)
 
+# Output
 print "Errori: " + str(errorRDD.count())
 print "Warning: " + str(warningRDD.count())
